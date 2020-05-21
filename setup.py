6bae45080c755e22da76877b10769309f4a485e0
Kristof <kristof.kovacs@telenav.com>
2020-05-21 22:50:31 +0300

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kffmpeg",
-    version="0.1.4",
+    version="0.2.5",
     author="Kovacs Kristof-Attila",
     description="kffmpeg",
     long_description=long_description,
